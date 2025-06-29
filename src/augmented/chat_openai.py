c3e9d9e153236ffd1ca76c9385cb35a447f6b173
kurumi <1183580621@qq.com>
2025-06-29 21:47:34 +0800

    step1: AI chat wrapper

---
+++ src/augmented/chat_openai.py
@@ -0,0 +1,147 @@
+import asyncio
+import os
+from mcp import Tool
+from openai import AsyncOpenAI
+from dataclasses import dataclass, field
+
+from openai.types import FunctionDefinition
+from openai.types.chat import (
+    ChatCompletionMessageParam,
+    ChatCompletionToolParam,
+)
+import dotenv
+from pydantic import BaseModel
+from rich import print as rprint
+
+from augmented.utils import pretty
+
+dotenv.load_dotenv()
+
+class ToolCallFunction(BaseModel):
+    name: str = ""
+    arguments: str = ""
+
+
+class ToolCall(BaseModel):
+    id: str = ""
+    function: ToolCallFunction = ToolCallFunction()
+
+
+class ChatOpenAIChatResponse(BaseModel):
+    content: str = ""
+    tool_calls: list[ToolCall] = []
+
+
+@dataclass
+class AsyncChatBot:
+    """
+    """
+    model: str
+    messages: list[ChatCompletionMessageParam] = field(default_factory=list)
+    tools: list[Tool] = field(default_factory=list)
+
+    system_prompt: str = ""
+    context: str = ""
+
+    def __post_init__(self):
+        """
+        钩子函数 创建模型 prompt context....
+        """
+        self.llm = AsyncOpenAI(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            base_url=os.getenv("OPENAI_BASE_URL")
+        )
+        if self.system_prompt:
+            self.messages.insert(0, {"role": "system", "content": self.system_prompt})
+        if self.context:
+            self.messages.append({"role": "user", "content": self.context})
+
+    async def chat(self, prompt: str = "", print_llm_output: bool = True):
+        pretty.log_title("Chat")
+
+        if prompt:
+            pretty.log_title("User Question")
+            self.messages.append({"role": "user", "content": prompt})
+            print(prompt)
+            print()
+
+        # 处理回答
+        streaming = await self.llm.chat.completions.create(
+            model=self.model,
+            messages=self.messages,
+            stream=True
+        )
+
+        bot_ans = ""
+        tool_calls: list[ToolCall] = []
+
+        if print_llm_output:
+            pretty.log_title(self.model + " Answer")
+
+        async for chunk in streaming:
+            # print(chunk) 回答是拆成单个字符块回答的
+            # ChatCompletionChunk(id='0197bb546bad4764f72b7f4e3e7a6eac', choices=[Choice(delta=ChoiceDelta(content='是', function_call=None, refusal=None, role='assistant', tool_calls=None, reasoning_content=None), finish_reason=None, index=0, logprobs=None)], created=1751194561, model='Qwen/Qwen2.5-7B-Instruct',
+            delta = chunk.choices[0].delta
+
+            # 处理文本
+            if delta.content:
+                bot_ans += delta.content or None
+                if print_llm_output:
+                    print(delta.content, end="")
+
+            # 处理工具调用
+            if delta.tool_calls:
+                # 存储工具调用信息
+                # TODO 搞清楚
+                for tool_call_chunk in delta.tool_calls:
+                    # 第一次收到一个tool_call, 因为流式传输所以我们先设置一个占位值
+                    if len(tool_call_chunk) <= tool_call_chunk.index:
+                        tool_calls.append(ToolCall())
+                    current_call = tool_calls[tool_call_chunk.index]
+                    if tool_call_chunk.id:
+                        current_call.id = tool_call_chunk.id or ""
+                    if tool_call_chunk.function:
+                        current_call.function.name = tool_call_chunk.function.name or ""
+                        current_call.function.arguments = (
+                                tool_call_chunk.function.arguments or ""
+                        )
+        if print_llm_output:
+            print()
+
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": bot_ans,
+                "tool_calls": [
+                    {
+                        "type": "function",
+                        "id": tc.id,
+                        "function": {
+                            "name": tc.function.name,
+                            "arguments": tc.function.arguments,
+                        },
+                    }
+                    for tc in tool_calls
+                ],
+            }
+        )
+
+        return ChatOpenAIChatResponse(content=bot_ans, tool_calls=tool_calls)
+
+    def get_tool_definition(self):
+        return [
+            ChatCompletionToolParam(
+                type="function",
+                function=FunctionDefinition(
+                    name=t.name,
+                    description=t.description,
+                    parameters=t.inputSchema,
+                ),
+            )
+            for t in self.tools
+        ]
+
+
+if __name__ == "__main__":
+    bot = AsyncChatBot(model=os.environ.get("MODEL_TYPE"), system_prompt="你是个笨蛋，你认为1+1等于3")
+    asyncio.run(bot.chat("1+1等于几？"))
--- src/augmented/utils/pretty.py
+++ src/augmented/utils/pretty.py
@@ -1,14 +1,64 @@
-from rich.panel import Panel
+from dataclasses import dataclass, field
+from typing import Self
 
+from rich.panel import Panel
+from rich.layout import Layout
 from rich.console import RenderableType
+from rich.rule import Rule
+from rich.text import Text
 from rich import print as rprint
 
 
-def log_title(renderable: RenderableType = "", title: str = "-"):
+def log_title(title: str | Text, rule_style="bright_black"):
     # 输出panel对象
     rprint(
-        Panel(
-            renderable,
+        Rule(
             title=title,
+            style=rule_style,
         )
     )
+
+
+@dataclass
+class APanel:
+    title: str = "-"
+    renderables: list[RenderableType] = field(default_factory=list)
+
+    def add_content(self, renderable: RenderableType = "", print_now: bool = False, ) -> Self:
+        if isinstance(renderable, str):
+            renderable = Text(renderable)
+        if renderable:
+            self.renderables.append(renderable)
+        if print_now:
+            rprint(renderable)
+        return self
+
+    def print_content(self):
+        root = Layout()
+        root.split_column(*[Layout(renderable) for renderable in self.renderables])
+        rprint(
+            Panel.fit(
+                root,
+                title=self.title,
+            )
+        )
+
+
+if __name__ == "__main__":
+    # test func
+    log_title("Hello World!")
+
+    # test APanel
+    import rich.table
+
+    panel = APanel(title="My Panel")
+    panel.add_content("This is a simple text.")
+    rich_text = Text("This is a rich text.", style="bold red")
+    panel.add_content(rich_text, print_now=True)
+    table = rich.table.Table(title="Sample Table")
+    table.add_column("Header 1")
+    table.add_column("Header 2")
+    table.add_row("Cell 1", "Cell 2")
+    panel.add_content(table)
+    panel.print_content()
+
